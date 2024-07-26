20524149195190c8e576bbeea750b0dd216ccd46
quezandoc <quezandoc@gmail.com>
2024-07-26 13:00:27 -0400

    pls test middleware

--- app/bootstrap/app.php
+++ app/bootstrap/app.php
@@ -7,14 +7,14 @@ use App\Http\Middleware\JsonResponseMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->add(JsonResponseMiddleware::class);
-        //
+        // Registrar el middleware globalmente
+        $middleware->append(JsonResponseMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
